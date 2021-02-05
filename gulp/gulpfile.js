a6fc6df99f516b2fcb19f9a416aaae4e160e09da
DJ1TJOO <t.ferb1@gmail.com>
2021-02-05 09:27:40 +0100

    Added webpack

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,29 +1,27 @@
 let gulp = require("gulp");
-let sass = require("gulp-dart-sass");
+let sass = require("gulp-sass");
 let browserSync = require("browser-sync").create();
+
 const $ = require("gulp-load-plugins")({
     scope: ["devDependencies"],
     pattern: "*",
 });
+
+function requireUncached(module) {
+    delete require.cache[require.resolve(module)];
+    return require(module);
+}
 
 gulp.task("clean", () => {
     return gulp.src("../build", { read: false, allowEmpty: true }).pipe($.clean({ force: true }));
 });
 
 gulp.task("sass", () => {
-    return gulp.src("../src/css/main.scss").pipe(sass()).pipe(gulp.dest("../build/res/"));
+    return gulp.src("../src/css/main.scss").pipe(sass()).pipe(gulp.dest("../build/"));
 });
 
 gulp.task("sass.watch", () => {
-    return gulp
-        .src("../src/css/main.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("../build/res/"))
-        .pipe(
-            browserSync.reload({
-                stream: true,
-            })
-        );
+    return gulp.src("../src/css/main.scss").pipe(sass()).pipe(gulp.dest("../build/")).pipe(browserSync.stream());
 });
 
 gulp.task("html", () => {
@@ -64,32 +62,40 @@ gulp.task("html.watch", () => {
         )
         .pipe($.htmlBeautify())
         .pipe(gulp.dest("../build/"))
-        .pipe(
-            browserSync.reload({
-                stream: true,
-            })
-        );
+        .pipe(browserSync.stream());
 });
 
 gulp.task("js", () => {
-    return;
+    return gulp
+        .src("../src/js/main.js")
+        .pipe($.webpackStream(requireUncached("./webpack.config.js")()))
+        .pipe(gulp.dest("../build/"));
 });
 
 gulp.task("js.watch", () => {
-    return;
+    return gulp
+        .src("../src/js/main.js")
+        .pipe(
+            $.webpackStream(
+                requireUncached("./webpack.config.js")({
+                    watch: true,
+                })
+            )
+        )
+        .pipe(gulp.dest("../build/"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task("browserSync", function() {
-    return browserSync.init({
+gulp.task("build", gulp.series(["clean", gulp.parallel(["sass", "html", "js"])]));
+
+gulp.task("watch", () => {
+    gulp.task("build");
+    browserSync.init({
         server: {
             baseDir: "../build",
         },
+        notify: false,
     });
-});
-
-gulp.task("build", gulp.series(["clean", "sass", "html", "js"]));
-
-gulp.task("watch", gulp.series(["build", "browserSync"]), () => {
     gulp.watch("../src/css/**/*.scss", gulp.series(["sass.watch"]));
     gulp.watch("../src/js/**/*.js", gulp.series(["js.watch"]));
     gulp.watch("../src/html/index.html", gulp.series(["html.watch"]));
